2e50a7dab5bfd1c5fe52abb8c24d1de2ae5261c7
Josh Perez <josh@goatslacker.com>
2015-05-12 03:27:03 -0700

    Remove PUBLIC_METHODS symbol

--- src/alt/store/StoreMixin.js
+++ src/alt/store/StoreMixin.js
@@ -27,7 +27,7 @@ const StoreMixin = {
         throw new TypeError('exportPublicMethods expects a function')
       }
 
-      this[Sym.PUBLIC_METHODS][methodName] = value
+      this.publicMethods[methodName] = value
     }, [methods])
   },
 
